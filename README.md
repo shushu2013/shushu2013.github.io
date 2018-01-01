1bdb878092482e2841cfb1b045217f9bb7c375ec
wwb <1252093436@qq.com>
2018-01-01 12:59:52 +0800

    Site updated: 2018-01-01 12:59:51

---
+++ README.md
@@ -0,0 +1,6 @@
+## Hexo brisklife 主题博客
+
+#### 学习、记录、成长
+
+[博客](https://shushu2013.github.io)  
+[技术笔记](https://github.com/shushu2013/shushu2013.github.io/issues)
--- js/index.js
+++ js/index.js
@@ -1,43 +1,86 @@
 // side
 $(function() {
-    var side = $('.side');
-    var action = $('.side-action')
+    'use strict'
+
+    var side = $('.c-side')
+    var action = $('.c-action.js-action')
 
     $('html').on('click', function(e) {
-        var target = $(e.target);
+        var target = $(e.target)
         // console.log(target);
         // console.log(target.is(side));
-        if(!side.hasClass('hiddened') && !target.is(action)) {
-            side.addClass('hiddened');
+        if(!side.hasClass('is-hidden') && !target.is(action)) {
+            side.addClass('is-hidden')
         }
 
         if(target.is(action)) {
-            side.toggleClass('hiddened');
+            side.toggleClass('is-hidden')
         }
+        // console.log('click')
+    })
+})
+
+// move
+$(function() {
+    'use strict'
+
+    var side = $('.c-side')
+    var action = $('.c-action.js-action')
+    // 记录上一次 touch 位置
+    var touch_clientX
+    var touch_clientY
+    
+    action.on('touchstart', function(e) { 
+        var touch = e.originalEvent.changedTouches[0]
+
+        touch_clientX = touch.clientX
+        touch_clientY = touch.clientY
+    })
+
+    action.on('touchmove', function(e) {
+        var touch = e.originalEvent.changedTouches[0]
+        var action_right = Number.parseInt(action.css('right')) 
+        var action_bottom = Number.parseInt(action.css('bottom'))
+
+        e.preventDefault()  // 阻止默认事件（解决移动端页面滑动）
+        // e.stopPropagation() // 阻止事件冒泡
+
+        // 计算该偏移的位置
+        var right = action_right + (touch_clientX - touch.clientX)
+        var bottom = action_bottom + (touch_clientY - touch.clientY)
+        
+        // 保留上一次 touch 位置
+        touch_clientX = touch.clientX
+        touch_clientY = touch.clientY
+        
+        // side.text(action.css('right'))
+
+        action.css('right', right + 'px')
+        action.css('bottom', bottom + 'px')
     })
 })
 
 // hobby
 $(function(){
-    var nav = $('.hobby .nav');
-    var span = $('.hobby .nav span');
-    var div = $('.hobby .list div[data-category]')
+    'use strict'
+
+    var nav = $('.c-hobby .c-hobby__nav')
+    var span = $('.c-hobby .js-hobby-change')
+    var div = $('.c-hobby .js-hobby-trigger')
 
     nav.on('click', function(e) {
-        var item = $(e.target);
-        // console.log(item);
-        // console.log(span.is(item));
+        var item = $(e.target)
+        // console.log(item)
+        // console.log(span.is(item))
 
         if(span.is(item)) {
-            var $div = $('.hobby .list div[data-category="' + item.attr('data-category') + '"]');
+            var $div = $('.c-hobby div[data-category="' + item.attr('data-category') + '"]')
 
-            span.removeClass('actived');
-            item.addClass('actived');
+            span.removeClass('is-hobby-active')
+            item.addClass('is-hobby-active')
 
-            div.removeClass('no-hidden');
-            div.addClass('hidden');
-            $div.addClass('no-hidden');
-            $div.removeClass('hidden');
+            div.addClass('is-hidden')
+            $div.removeClass('is-hidden')
         }
     })
 })
