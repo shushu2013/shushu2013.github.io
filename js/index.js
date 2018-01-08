91388cd5b693c399b503ee48e174c604d625be75
wwb <1252093436@qq.com>
2018-01-08 13:32:37 +0800

    Site updated: 2018-01-08 13:32:36

--- js/index.js
+++ js/index.js
@@ -12,10 +12,7 @@ $(function() {
 
     $('html').on('click', function(e) {
         var target = $(e.target)
-        // console.log(e)
-        // console.log(target)
-        // console.log(target.is(side));
-        // $('.js-test').html('click time: ' + (++num) )
+        
         if(!side.hasClass('is-hidden') && !target.is(action) ) {
             side.addClass('is-hidden')
         }
@@ -35,34 +32,37 @@ $(function() {
     // 记录上一次 touch 位置
     var touch_clientX
     var touch_clientY
+    var action_right
+    var action_bottom
+
     // 屏幕宽度和高度
     var screenHeight = $(window).height();  // screen.height
     var screenWidth = $(window).width();    // screen.width
-    // console.log("h: " + screenHeight + " w: " + screenWidth)
     
-    action.on('touchstart', function(e) { 
+    action.on('touchstart', function(e) {
         var touch = e.originalEvent.changedTouches[0]
 
         touch_clientX = touch.clientX
         touch_clientY = touch.clientY
+
+        action_right = Number.parseFloat(action.css('right'))
+        action_bottom = Number.parseFloat(action.css('bottom'))
     })
 
     action.on('touchmove', function(e) {
         var touch = e.originalEvent.changedTouches[0]
-        var action_right = Number.parseFloat(action.css('right'))
-        var action_bottom = Number.parseFloat(action.css('bottom'))
         var WBOUND = 15; // 边界范围控制
-        var HBOUND = 100;
+        var HBOUND = 15;
 
         e.preventDefault()  // 阻止默认事件（解决移动端页面滑动）
         // e.stopPropagation() // 阻止事件冒泡
 
 
         // 超出屏幕预设边界，直接返回
-        if(touch.clientX < WBOUND || touch.clientX > screenWidth - WBOUND) {
+        if(touch.clientX < WBOUND || touch.clientX > (screenWidth - WBOUND)) {
             return
         }
-        if(touch.clientY < HBOUND || touch.clientY > screenHeight - HBOUND) {
+        if(touch.clientY < HBOUND || touch.clientY > (screenHeight - HBOUND)) {
             return
         }
 
@@ -70,11 +70,6 @@ $(function() {
         var right = action_right + (touch_clientX - touch.clientX)
         var bottom = action_bottom + (touch_clientY - touch.clientY)
         
-        // 保留上一次 touch 位置
-        touch_clientX = touch.clientX
-        touch_clientY = touch.clientY
-
-        console.log(touch)
         action.css('right', right + 'px')
         action.css('bottom', bottom + 'px')
     })
@@ -90,8 +85,6 @@ $(function(){
 
     nav.on('click', function(e) {
         var item = $(e.target)
-        // console.log(item)
-        // console.log(span.is(item))
 
         if(span.is(item)) {
             var $div = $('.c-hobby div[data-category="' + item.attr('data-category') + '"]')
